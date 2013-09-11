ccf31abc9fd2a642280d40d0e82a0511eea1ca82
Rémi Attab <remi@datacratic.com>
2013-09-11 14:38:07 -0400

    Reordered the filter priorities to match the legacy filters.
    Necessary to properly compare the two filtering schemes.

--- core/router/filters/priority.h
+++ core/router/filters/priority.h
@@ -17,29 +17,29 @@ namespace RTBKIT {
 
 struct Priority
 {
-    static constexpr unsigned ExchangePre          = 0x0100;
-    static constexpr unsigned ExchangeName         = 0x0200;
+    static constexpr unsigned ExchangePre          = 0x0010;
 
-    static constexpr unsigned Location             = 0x1000;
-    static constexpr unsigned Language             = 0x1100;
-    static constexpr unsigned Host                 = 0x1200;
-    static constexpr unsigned Url                  = 0x1300;
-
-    static constexpr unsigned CreativeFormat       = 0x2000;
-    static constexpr unsigned CreativeLocation     = 0x2100;
-    static constexpr unsigned CreativeExchangeName = 0x2200;
-    static constexpr unsigned CreativeLanguage     = 0x2300;
-
-    static constexpr unsigned Segments             = 0x3000;
-    static constexpr unsigned HourOfWeek           = 0x3100;
-    static constexpr unsigned FoldPosition         = 0x3200;
-    static constexpr unsigned RequiredIds          = 0x3300;
-    static constexpr unsigned UserPartition        = 0x3400;
+    static constexpr unsigned FoldPosition         = 0x1100;
+    static constexpr unsigned CreativeFormat       = 0x1200;
+    static constexpr unsigned CreativeLocation     = 0x1300;
+    static constexpr unsigned CreativeExchangeName = 0x1400;
+    static constexpr unsigned CreativeLanguage     = 0x1500;
 
     // Really slow so delay as much as possible.
-    static constexpr unsigned CreativeExchange     = 0xF100;
+    static constexpr unsigned CreativeExchange     = 0x1600;
 
-    static constexpr unsigned ExchangePost         = 0xFF00;
+    static constexpr unsigned RequiredIds          = 0x3000;
+    static constexpr unsigned HourOfWeek           = 0x4000;
+    static constexpr unsigned ExchangeName         = 0x5000;
+    static constexpr unsigned Location             = 0x6000;
+    static constexpr unsigned Language             = 0x7000;
+
+    static constexpr unsigned Segments             = 0x8000;
+    static constexpr unsigned UserPartition        = 0x8010;
+    static constexpr unsigned Host                 = 0x8500;
+    static constexpr unsigned Url                  = 0x9000;
+
+    static constexpr unsigned ExchangePost         = 0x9900;
 };
 
 
